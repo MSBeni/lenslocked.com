49410314a13d915b8ba453e14dd73332323940ee
MSBeni <andrei.sokurov.bitco@gmail.com>
2020-04-01 17:05:58 -0400

    Added a content-type header

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import(
 	"fmt"
 	"net/http"
 )
-
+//#########################################BASIC GO SERVER FILE#########################################################
 // Please check the https://github.com/gravityblast/fresh to install the fresh package for dynamic reloading and conf file
 func HandlerFunc(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w,"<h1>Hello. This is the most easiest simple start you can have </h1>")
 }
 func main(){
 	http.HandleFunc("/", HandlerFunc)
 	http.ListenAndServe(":3000", nil)
 }
+//######################################################################################################################
