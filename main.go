4b16158cfe7dac73df433f74c4661933a6a8ba43
MSBeni <andrei.sokurov.bitco@gmail.com>
2020-04-03 18:14:52 -0400

    Added a contact page

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import(
+import (
 	"fmt"
 	"net/http"
 )
@@ -8,7 +8,12 @@ import(
 // Please check the https://github.com/gravityblast/fresh to install the fresh package for dynamic reloading and conf file
 func HandlerFunc(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w,"<h1>Hello. This is the most easiest simple start you can have </h1>")
+	if r.URL.Path == "/"{
+		fmt.Fprint(w,"<h1>Hello. This is the most easiest simple start you can have </h1>")
+	}else if r.URL.Path == "/contact"{
+		fmt.Fprint(w,"To get in touch, please send an email to <a href \"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
+	}
+
 }
 func main(){
 	http.HandleFunc("/", HandlerFunc)
