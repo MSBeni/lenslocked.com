0c58331c60fa1f06d9775c1d8008b495c57cdfd6
MSBeni <andrei.sokurov.bitco@gmail.com>
2020-04-03 18:28:56 -0400

    Added a 404 page

--- main.go
+++ main.go
@@ -12,6 +12,9 @@ func HandlerFunc(w http.ResponseWriter, r *http.Request){
 		fmt.Fprint(w,"<h1>Hello. This is the most easiest simple start you can have </h1>")
 	}else if r.URL.Path == "/contact"{
 		fmt.Fprint(w,"To get in touch, please send an email to <a href \"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
+	}else{
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>We could not find the page you are looking for</h1> <p> Please email us via /contact page to solve the problem</p>")
 	}
 
 }
